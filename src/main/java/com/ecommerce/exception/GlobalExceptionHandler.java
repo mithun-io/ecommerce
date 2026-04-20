2f62563d36cfd3189a0846f897240e5a716a6fa9
mithun-io <mithun-io@outlook.com>
2026-04-20 18:34:22 +0530

    validation

--- src/main/java/com/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/exception/GlobalExceptionHandler.java
@@ -4,11 +4,22 @@ import com.ecommerce.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, Object>>> handle(MethodArgumentNotValidException exception) {
+        Map<String, Object> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(x -> errors.put(x.getField(), x.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(false, "validation failed", errors, 400));
+    }
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<ApiResponse<String>> handle(NoResourceFoundException exception) {
